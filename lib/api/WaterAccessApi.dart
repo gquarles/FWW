56f24d67cd7d1a27d4bc4e4cd5d50094bf004390
Griffin <gquarles@protonmail.com>
2020-08-19 14:31:58 -0400

    Refactor

---
+++ lib/api/WaterAccessApi.dart
@@ -0,0 +1,121 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class WaterAccessApi {
+  static const String url =
+      'https://app.fw.ky.gov/fishingboatingwebapi/api/accessSites/details';
+
+  static const Map<String, String> headers = {
+    'Content-type': 'application/json',
+    'Accept': 'application/json',
+  };
+
+  Future<List<AccessSite>> getAllAcessSites() async {
+    var body = json.encode({
+      "northEastLat": 102.81401063830785,
+      "northEastLng": -44.617968556271705,
+      "southWestLat": -17.074970707553526,
+      "southWestLng": -132.61403304680803,
+      "userLat": 38.141783771370896,
+      "userLng": -84.85246857970064
+    });
+
+    var response = await http.post(url, body: body, headers: headers);
+
+    if (response.statusCode != 200) return List<AccessSite>();
+
+    final sitesJson = json.decode(response.body);
+
+    List<AccessSite> sites = List<AccessSite>();
+    for (var siteJson in sitesJson['Result']) {
+      AccessSite site = AccessSite(
+        id: siteJson['accessSiteDetails']['accessSite_ID'],
+        type: siteJson['accessSiteDetails']['accessType'],
+        typeCode: siteJson['accessSiteDetails']['accessTypeCode'],
+        accessSurface: siteJson['accessSiteDetails']['accessSurface'],
+        modified: siteJson['accessSiteDetails']['Modified'],
+        name: siteJson['accessSiteDetails']['accessSiteName'],
+        fee: siteJson['accessSiteDetails']['accessFee'],
+        notes: siteJson['accessSiteDetails']['accessNotes'],
+        lat: siteJson['accessSiteDetails']['latitude'],
+        lng: siteJson['accessSiteDetails']['longitude'],
+        county: siteJson['accessSiteDetails']['accessCounty'],
+        shoreLine: siteJson['accessSiteDetails']['shoreLine'],
+        boatRamp: siteJson['accessSiteDetails']['boatRamp'],
+        restrooms: siteJson['accessSiteDetails']['restrooms'],
+        waterBodyId: siteJson['accessSiteDetails']['waterBody_ID'],
+        handicapRestrooms: siteJson['accessSiteDetails']['handicapRestrooms'],
+        handicapParking: siteJson['accessSiteDetails']['handicapParking'],
+        handicapPier: siteJson['accessSiteDetails']['handicapPier'],
+        handicapRamp: siteJson['accessSiteDetails']['handicapRamp'],
+        kdfwrFlag: siteJson['accessSiteDetails']['kdfwrFlag'],
+        imgUrl: siteJson['imageDetails'][0]['imageLocation'],
+        imgDesc: siteJson['imageDetails'][0]['imageDescription'],
+        capacity: siteJson['accessSiteDetails']['capacity'],
+        waterBodyName: siteJson['waterbodyDetails'][0]['name'],
+      );
+      
+      sites.insert(0, site);
+    }
+
+    return sites;
+  }
+}
+
+class AccessSite {
+  final int id;
+  final String type;
+  final String typeCode;
+  final String capacity;
+  final String accessSurface;
+  final String modified;
+  final String name;
+  final bool fee;
+  final String notes;
+  final double lat;
+  final double lng;
+  final String county;
+  final String shoreLine;
+  final String boatRamp;
+  final String restrooms;
+  final int waterBodyId;
+  final String handicapRestrooms;
+  final String handicapParking;
+  final String handicapPier;
+  final String handicapRamp;
+  final bool kdfwrFlag;
+  final int imgId;
+  final String imgUrl;
+  final String imgDesc;
+  final String waterBodyName;
+
+  AccessSite({
+    this.id,
+    this.type,
+    this.typeCode,
+    this.capacity,
+    this.accessSurface,
+    this.modified,
+    this.name,
+    this.fee,
+    this.notes,
+    this.lat,
+    this.lng,
+    this.county,
+    this.shoreLine,
+    this.boatRamp,
+    this.restrooms,
+    this.waterBodyId,
+    this.handicapRestrooms,
+    this.handicapParking,
+    this.handicapPier,
+    this.handicapRamp,
+    this.kdfwrFlag,
+    this.imgId,
+    this.imgUrl,
+    this.imgDesc,
+    this.waterBodyName,
+  });
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'dart:async';
-import 'dart:convert';
 
+import 'package:FWW/api/WaterAccessApi.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -23,65 +22,37 @@ class MapSample extends StatefulWidget {
 }
 
 class MapSampleState extends State<MapSample> {
-  Map<MarkerId, Marker> markers =
-      <MarkerId, Marker>{}; // CLASS MEMBER, MAP OF MARKS
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  Widget loadingWidget = CircularProgressIndicator();
 
-  void _add(String markerIdVal, String snippet, lat, lng) {
-    final MarkerId markerId = MarkerId(markerIdVal);
+  _onMapCreated(GoogleMapController controller) async {
+    WaterAccessApi api = WaterAccessApi();
 
-    // creating a new MARKER
-    final Marker marker = Marker(
-      markerId: markerId,
-      position: LatLng(lat, lng),
-      infoWindow: InfoWindow(
-          title: markerIdVal, snippet: snippet, onTap: () => print('Tapped')),
-      onTap: () {
-        print(markerId);
-      },
-    );
+    List<AccessSite> sites = await api.getAllAcessSites();
 
-    setState(() {
-      // adding a new marker to map
+    markers.clear();
+    for (AccessSite site in sites) {
+      final MarkerId markerId = MarkerId(site.name);
+      final Marker marker = Marker(
+        markerId: markerId,
+        position: LatLng(site.lat, site.lng),
+        infoWindow: InfoWindow(
+          title: site.name,
+          snippet: site.type,
+        ),
+      );
       markers[markerId] = marker;
-    });
-  }
-
-  _getAllAccessSites() async {
-    const String url =
-        'https://app.fw.ky.gov/fishingboatingwebapi/api/accessSites/details';
-
-    var body = json.encode({
-      "northEastLat": 38.81088413602677,
-      "northEastLng": -83.85073424186862,
-      "southWestLat": 34.95122227420873,
-      "southWestLng": -86.17673735140696,
-      "userLat": 38.141783771370896,
-      "userLng": -84.85246857970064
-    });
-
-    Map<String, String> headers = {
-      'Content-type': 'application/json',
-      'Accept': 'application/json',
-    };
-
-    var response = await http.post(url, body: body, headers: headers);
-
-    final responseJson = json.decode(response.body);
-
-    //print(responseJson['Result']);
-
-    for (var site in responseJson['Result']) {
-      _add(site['accessSiteDetails']['accessSiteName'], site['accessSiteDetails']['accessType'], site['accessSiteDetails']['latitude'], site['accessSiteDetails']['longitude']);
     }
+    setState(() {
+      markers = markers;
+      loadingWidget = Container();
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    _getAllAccessSites();
   }
-
-  Completer<GoogleMapController> _controller = Completer();
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(38.34306, -84.03564),
@@ -91,13 +62,20 @@ class MapSampleState extends State<MapSample> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.normal,
-        markers: Set<Marker>.of(markers.values),
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            markers: markers.values.toSet(),
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: _onMapCreated
+          ),
+          Center(
+            child: Container(
+              child: loadingWidget,
+            ),
+          ),
+        ],
       ),
     );
   }
